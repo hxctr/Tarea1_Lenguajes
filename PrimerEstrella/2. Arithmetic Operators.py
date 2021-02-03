36204e68a3f25949cc69e606c877e3edb10ed573
Hector Ponsoy <hectorjosue65@gmail.com>
2021-02-03 17:46:05 -0600

    Primeros 2 programas

---
+++ PrimerEstrella/2. Arithmetic Operators.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    
+    mult = a * b
+    suma = a  + b
+    rest = a - b
+
+   
+    print(suma)
+    print(rest)
+    print(mult)
